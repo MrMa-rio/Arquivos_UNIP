2b7f502832776c5d304b2f332c72174ecfb7dc2b
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-03-10 12:11:06 -0300

    Banco_de_dados

---
+++ POO/C#/Teste/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] semana = { "domingo", "segunda", "terça", "quarta", "quinta", "sexta" };
+            string[] newSemana = 
+            foreach (string s in semana)
+            {
+                Console.WriteLine(s);
+            }
+            for(int i = semana.Length - 1; i >= 0; i--)
+            {
+                newSemana.Append(semana[i]);
+
+            }
+            foreach(string s in newSemana)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+    }
+}
